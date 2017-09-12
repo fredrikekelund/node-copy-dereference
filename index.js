7a044793df983837146a1f6883dea4a882e2cc7f
Fredrik Ekelund <fredrik@fredrik.computer>
2017-09-12 11:51:10 +0200

    Started fleshing out mtime diffing between src and dest

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ function copyDereferenceSync (src, dest) {
   // We could try readdir'ing and catching ENOTDIR exceptions, but that is 3x
   // slower than stat'ing in the common case that we have a file.
   var srcStats = fs.statSync(src)
+
+  try {
+    var destStats = fs.statSync(path.resolve(process.cwd(), dest))
+
+    if (srcStats.mtime <= destStats.mtime) {
+      return;
+    }
+  } catch (error) {
+    // Assume that file didn't exist, do nothing
+  }
+
   if (srcStats.isDirectory()) {
     // We do not copy the directory mode by passing a second argument to
     // mkdirSync, because we wouldn't be able to populate read-only
@@ -19,10 +30,11 @@ function copyDereferenceSync (src, dest) {
     fs.mkdirSync(dest)
     var entries = fs.readdirSync(src).sort()
     for (var i = 0; i < entries.length; i++) {
-      copyDereferenceSync(src + path.sep + entries[i], dest + path.sep + entries[i])
+      copyDereferenceSync(path.join(src, entries[i]), path.join(dest, entries[i]))
     }
   } else if (srcStats.isFile()) {
     var contents = fs.readFileSync(src)
+    fs.unlinkSync(dest)
     fs.writeFileSync(dest, contents, { flag: 'wx', mode: srcStats.mode })
   } else {
     throw new Error('Unexpected file type for ' + src)
